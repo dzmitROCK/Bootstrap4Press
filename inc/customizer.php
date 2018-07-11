edf79c52ec3654fe4a7b8f1a92869e5aa38440c7
dzmitrock <besfamilny@dzmitrock.ru>
2018-07-11 12:34:05 +0300

    clean js

--- inc/customizer.php
+++ inc/customizer.php
@@ -50,6 +50,6 @@ function bootstrap4press_customize_partial_blogdescription() {
  * Binds JS handlers to make Theme Customizer preview reload changes asynchronously.
  */
 function bootstrap4press_customize_preview_js() {
-	wp_enqueue_script( 'bootstrap4press-customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20151215', true );
+	wp_enqueue_script( 'bootstrap4press-customizer', get_template_directory_uri() . '/src/js/libs/customizer.js', array( 'customize-preview' ), '20151215', true );
 }
 add_action( 'customize_preview_init', 'bootstrap4press_customize_preview_js' );
